8b0a43ce011148c371cd21adc649694a88f4db3f
elseba27 <82976539+elseba27@users.noreply.github.com>
2021-09-15 12:36:24 -0300

    Add files via upload

---
+++ ServiceRequestSln/ServiceRequestApplication/Models/ServiceRequest.cs
@@ -0,0 +1,33 @@
+﻿using ServiceRequestApplication.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceRequestApplication.Models
+{
+    public class ServiceRequest
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string BuildingCode { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public CurrentStatus CurrentStatus { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+    }
+}
